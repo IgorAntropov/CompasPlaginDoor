36e1393c3d389b05cae6dfc5c7adca2b3b802fab
Igor' Antropov <Igor' Antropov@IGOR>
2017-12-05 18:35:35 +0700

    add 1

--- KompasLib/Kompas.cs
+++ KompasLib/Kompas.cs
@@ -93,14 +93,14 @@ namespace KompasLib
 
             floorKeyDifinition.EndEdit();
 
-            Extrude(doorParameters, doorPart, floorKeySketch, 7, (short) Direction_Type.dtNormal);
+            Extrude(doorParameters, doorPart, floorKeySketch, 15, (short) Direction_Type.dtNormal);
 
             //"основание ручки"->"ручка"
             ksEntity keyPlaneOffset = doorPart.NewEntity((short) Obj3dType.o3d_planeOffset);
             //создаем переменную смещенной поверхности 
             ksPlaneOffsetDefinition pod5 = keyPlaneOffset.GetDefinition(); // получаем указатель на её настройки 
             pod5.SetPlane(eyePlaneOffset); // ХУ плоскость установим как исходную, чтобы отталкиватся от неё 
-            pod5.offset = 7; //смещаемся на десять 
+            pod5.offset = 15; //смещаемся на 15 
             keyPlaneOffset.Create(); // создаем саму плоскость 
             ksEntity keySketch = doorPart.NewEntity((short) Obj3dType.o3d_sketch);
             //создаем переменную эскиза 
--- UnitTest/DoorParametrsTest.cs
+++ UnitTest/DoorParametrsTest.cs
@@ -9,7 +9,7 @@ namespace UnitTest
     [TestFixture]
     public class DoorParametrsTest
     {
-        [TestCase(2000, 801, 60, 200, 1000, TestName = "(Positive)DoorParametrsConstructor")]
+        [TestCase(2000, 800, 60, 200, 1000, TestName = "(Positive)DoorParametrsConstructor")]
         [Test]
         public void TestPositiveDoorParametrsConstructor(int heightDoor, int widthDoor, int weigthDoor, int yKey,
             int yEye)
@@ -17,13 +17,58 @@ namespace UnitTest
             Assert.DoesNotThrow(() => { new DoorParameters(heightDoor, widthDoor, weigthDoor, yKey, yEye); });
         }
 
-        [TestCase(-100, -50, -3, -5, -100, TestName = "(Negative)DoorParametrsConstructor")]
+        [TestCase(-2000, -800, -60, -200, -1000, TestName = "(Negative)DoorParametrsConstructor")]
         [Test]
         public void TestNegativeDoorParametrsConstructor(int heightDoor, int widthDoor, int weigthDoor, int yKey,
             int yEye)
         {
             Assert.That(() => { new DoorParameters(heightDoor, widthDoor, weigthDoor, yKey, yEye); },
-                Throws.TypeOf(typeof (ArgumentException)));
+                Throws.TypeOf(typeof(ArgumentException)));
+        }
+
+        [TestCase(2000, -800, -60, -200, -1000, TestName = "(Negative)DoorParametrsHeight")]
+        [Test]
+        public void TestNegativeDoorParametrsHeight(int heightDoor, int widthDoor, int weigthDoor, int yKey,
+            int yEye)
+        {
+            Assert.That(() => { new DoorParameters(heightDoor, widthDoor, weigthDoor, yKey, yEye); },
+                Throws.TypeOf(typeof(ArgumentException)));
+        }
+
+        [TestCase(-2000, 800, -60, -200, -1000, TestName = "(Negative)DoorParametrsWidthr")]
+        [Test]
+        public void TestNegativeDoorParametrsWidth(int heightDoor, int widthDoor, int weigthDoor, int yKey,
+            int yEye)
+        {
+            Assert.That(() => { new DoorParameters(heightDoor, widthDoor, weigthDoor, yKey, yEye); },
+                Throws.TypeOf(typeof(ArgumentException)));
+        }
+
+        [TestCase(-2000, -800, 60, -200, -1000, TestName = "(Negative)DoorParametrsWeigtht")]
+        [Test]
+        public void TestNegativeDoorParametrsWeigtht(int heightDoor, int widthDoor, int weigthDoor, int yKey,
+            int yEye)
+        {
+            Assert.That(() => { new DoorParameters(heightDoor, widthDoor, weigthDoor, yKey, yEye); },
+                Throws.TypeOf(typeof(ArgumentException)));
+        }
+
+        [TestCase(-2000, -800, -60, 200, -1000, TestName = "(Negative)DoorParametrsYKey")]
+        [Test]
+        public void TestNegativeDoorParametrsYkey(int heightDoor, int widthDoor, int weigthDoor, int yKey,
+            int yEye)
+        {
+            Assert.That(() => { new DoorParameters(heightDoor, widthDoor, weigthDoor, yKey, yEye); },
+                Throws.TypeOf(typeof(ArgumentException)));
+        }
+
+        [TestCase(-2000, -800, -60, -200, 1000, TestName = "(Negative)DoorParametrsYEye")]
+        [Test]
+        public void TestNegativeDoorParametrsYeye(int heightDoor, int widthDoor, int weigthDoor, int yKey,
+            int yEye)
+        {
+            Assert.That(() => { new DoorParameters(heightDoor, widthDoor, weigthDoor, yKey, yEye); },
+                Throws.TypeOf(typeof(ArgumentException)));
         }
     }
 }
